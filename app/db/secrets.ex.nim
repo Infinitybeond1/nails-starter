3b6e25e1b74f5ad8622aa30592dc9bbd0e9d1738
Luke <Infinitybeond1@protonmail.com>
2022-09-25 11:21:05 -0500

    chore: finish secrets.nim comments

--- app/db/secrets.ex.nim
+++ app/db/secrets.ex.nim
@@ -1,5 +1,5 @@
 # Rename this file to secrets.nim and fill in the database information to use the database
-# If you dont want to use the database use `nimble bd`
+# If you dont want to use the database use `nimble bd` to build the project instead of `nimble b`
 
 const
   dbHost* = "databaseHost"
